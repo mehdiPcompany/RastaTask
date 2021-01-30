e7009c3ced1f3a53b7976f5400428bc6a3926e64
Mohammad Pasandideh <mohammadd.dev@gmail.com>
2021-01-30 10:31:38 +0330

    Finish Add Drawer to ListActivity

--- app/src/main/java/com/pas/rastatask/ListActivity.java
+++ app/src/main/java/com/pas/rastatask/ListActivity.java
@@ -1,14 +1,38 @@
 package com.pas.rastatask;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.LinearLayout;
 
 public class ListActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        drawerLayout1 = findViewById((R.id.drawerLayout));
+
+        LinearLayout pan_close = findViewById(R.id.pan_close);
+        pan_close.setOnClickListener(v -> {
+
+            if (drawerLayout1.isDrawerOpen(Gravity.RIGHT)){
+                drawerLayout1.closeDrawer(Gravity.RIGHT);
+            }else{
+                drawerLayout1.openDrawer(Gravity.RIGHT);
+            }
+        });
     }
+
+    @Override
+    public void onBackPressed() {
+        drawerLayout1.closeDrawer(Gravity.RIGHT);
+    }
+
+
 }
